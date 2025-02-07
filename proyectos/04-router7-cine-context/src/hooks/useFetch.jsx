d71a44f7fb9eb6f7eb9a3afe0afe2c4918f17546
isaiasfl <ifernandez@ieshlanz.es>
2025-02-07 14:25:28 +0100

    04-02-2025

--- proyectos/04-router7-cine-context/src/hooks/useFetch.jsx
+++
@@ -1,4 +0,0 @@
-// hook que se encargue de realizar cuaquier petición a una API
-export const useFetch = () => {
-  
-}
--- proyectos/04-router7-cine-context/src/pages/Home.jsx
+++ proyectos/04-router7-cine-context/src/pages/Home.jsx
@@ -1,51 +1,88 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import { useFetch } from "../hooks/useFetch";
+import { getPopularMovies } from "../services/tmdb";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const api_token = import.meta.env.VITE_API_TOKEN;
-  const fechingDataMovies = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${api_token}`
-    );
-    if (!response.ok) {
-      throw new Error("Error en la petición");
-    }
-    const data = await response.json();
-    setMovies(data.results);
+  // estado para el número de página
+  const [page, setPage] = useState(1);
+  // me traigo la data de las películas
+  const { data, loading, error } = useFetch(
+    () => getPopularMovies(page),
+    [page]
+  );
+  // que pasa con con el scroll
+
+  const handlePageChange = (newPage) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setPage(newPage);
   };
 
-  useEffect(() => {
-    fechingDataMovies();
-  }, []);
-
+  // si hay error cargando??
+  if (error) {
+    return (
+      <div className="text-center p-10">
+        <h2 className="text-red-600 text-2xl font-bold">
+          Error al traer las películas
+        </h2>
+        <p className="text-xl font-medium">{error.message}</p>
+        <Link to="/" className="text-blue-600">
+          Volver al inicio
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className="space-y-8">
-      {/* Cabecera  */}
       <header className="text-center">
         <h1 className="text-4xl font-bold text-sky-950">
-          Bienvenido a Movie App
+          Bienvenido a Videoclub
         </h1>
-        <p className="mt-2 text-gray-600">
-          La mejor aplicación para buscar películas y leer reseñas de cine
+        <p className="text-lg font-medium text-sky-900 mt-2">
+          Descubre las películas más populares del momento
         </p>
       </header>
-      {/* sección películas populares */}
+      {/* sección de películas populares */}
       <section>
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-bold">Películas populares</h2>
-          <Link to="/movies" className="text-sky-900 hover:underline">
-            Ver todas{" "}
-          </Link>
-        </div>
+        <h2 className="text-2xl font-bold text-sky-950 mb-10">
+          Películas Populares
+        </h2>
+        {loading ? (
+          <div> Cargando Películas...</div>
+        ) : (
+          <>
+            {/* grid de las películas */}
+            <div
+              className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
+            xl:grid-cols-5 gap-6"
+            >
+              {data?.results?.map((movie) => (
+                // aquí va el componente MovieCard
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </div>
+            {/* Paginación  */}
+            <div className="flex justify-center mt-8 gap-2">
+              <button
+                onClick={() => handlePageChange(page - 1)}
+                className=" text-white px-4 py-2 rounded-lg transition-colors duration-200 bg-sky-800 hover:bg-sky-950"
+                disabled={page ===1}
+              >
+                Anterior
+              </button>
+              <span></span>
+              <button
+                onClick={() => handlePageChange(page + 1)}
+                className="text-white px-4 py-2 rounded-lg transition-colors duration-200 bg-sky-800 hover:bg-sky-950"
+                disabled={page === data?.total_pages}
+              >
+                Siguiente
+              </button>
+            </div>
+          </>
+        )}
       </section>
-      {/* grid con las películas populares */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
-        {/* Películas */}
-        {movies?.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
     </div>
   );
 };
