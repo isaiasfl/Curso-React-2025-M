5c56026288d99100e39a31e11759104f07716c68
isaiasfl <ifernandez@ieshlanz.es>
2025-02-13 09:17:45 +0100

    13-02-2025

--- proyectos/05-jwt-frontend-backend/frontend/src/components/Navbar.jsx
+++ proyectos/05-jwt-frontend-backend/frontend/src/components/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { isAutenticated, login, checkAuth } = useAuth();
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white shadow-lg">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          <Link to="/" className="text-gray-800 text-xl font-bold p-4">
+            mi App
+          </Link>
+          {/* Menú navegación  */}
+          <div className="flex space-x-4">
+            <Link
+              to="/"
+              className="text-gray-800  hover:text-sky-800 px-4 py-2 rounded-md text-sm font-medium"
+            >
+              Inicio
+            </Link>
+            <Link
+              to="/login"
+              className="text-gray-800  hover:text-sky-800 px-4 py-2 rounded-md text-sm font-medium"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="text-gray-800  hover:text-sky-800 px-4 py-2 rounded-md text-sm font-medium"
+            >
+              Register
+            </Link>
+            <Link
+              to="/dashboard"
+              className="text-gray-800  hover:text-sky-800 px-4 py-2 rounded-md text-sm font-medium"
+            >
+              Dashboard
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- proyectos/05-jwt-frontend-backend/frontend/src/context/AuthContext.jsx
+++ proyectos/05-jwt-frontend-backend/frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -7,6 +7,25 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isAutenticated, setIsAutenticated] = useState(false);
   // funciones en mi contexto:
+  // Función para comprobar si estoy logueado o no
+  const checkAuth = async () => {
+    try {
+      const response = await fetch(`${VITE_BACKEND_URL}/auth/check-auth`, {
+        credentials: "include", // para indicar que se envíen las cookies al servidor
+      });
+      if (response.ok) {
+        setIsAutenticated(true);
+        return true;
+      } else {
+        throw new Error("No autenticado");
+      }
+    } catch (error) {
+      console.log("Error haciendo check-auth", error);
+      setIsAutenticated(false);
+      return false;
+    }
+  };
+
   // login -> para iniciar sesión
   const login = async (username, password) => {
     try {
@@ -32,4 +51,21 @@ export const AuthProvider = ({ children }) => {
   // checkAuth -> para verificar si el usuario está autenticado simpre que monte o renderice el componente
 
   // register -> para registrar un nuevo usuario
+
+  // provider de mi contexto
+  return (
+    <AuthContext.Provider value={{ isAutenticated, login, checkAuth }}>
+      {children}
+    </AuthContext.Provider>
+  )
+  
+};
+
+// exportamos el contexto como un Hook.
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth debe estar dentro del proveedor AuthProvider");
+  }
+  return context;
 };
